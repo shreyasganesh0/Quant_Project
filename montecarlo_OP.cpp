c620ab29291de5f63fe6c208154ada352e032918
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-09-22 16:07:07 -0400

    Create main method with parameters

---
+++ montecarlo_OP.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <vector>
+
+int main() {
+    // Option parameters
+    double S0 = 100.0;   // Initial stock price
+    double K = 100.0;    // Strike price
+    double r = 0.05;     // Risk-free rate
+    double sigma = 0.2;  // Volatility
+    double T = 1;      // Time to maturity (1 year)
+    int simulations = 100000; // Number of simulations
+
+   
+    return 0;
+}
