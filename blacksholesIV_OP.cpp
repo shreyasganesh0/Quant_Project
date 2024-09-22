1468e23b4cdb279669bdc45b043b0faf482f6245
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-09-22 18:42:10 -0400

    Add standard normal distribution function

--- blacksholesIV_OP.cpp
+++ blacksholesIV_OP.cpp
@@ -20,6 +20,31 @@ struct Contract
     double market_price; 
 };
 
+// Error function approximation
+double erf(double x) {
+    const double A1 = 0.254829592;
+    const double A2 = -0.284496736;
+    const double A3 = 1.421413741;
+    const double A4 = -1.453152027;
+    const double A5 = 1.061405429;
+    const double P = 0.3275911;
+
+    // Save the sign of x
+    int sign = (x >= 0) ? 1 : -1;
+    x = fabs(x);
+
+    // A&S formula 7.1.26
+    double t = 1.0 / (1.0 + P * x);
+    double y = 1.0 - (((((A5 * t + A4) * t) + A3) * t + A2) * t + A1) * t * exp(-x * x);
+
+    return sign * y;
+}
+
+// Cumulative standard normal density function
+double cumulativeStandardNormal(double x) {
+    return 0.5 * (1.0 + erf(x / sqrt(2.0)));
+}
+
 int main() {
     // Option parameters
     double S0 = 100.0;   // Initial stock price
